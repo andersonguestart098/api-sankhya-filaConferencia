4c0717f7cf7377099bb9f345e72183ce319e7d01
Anderson Henrique Tavares Anastácio <anderson.guestart98@gmail.com>
2025-11-24 15:08:28 -0300

    cors

---
+++ src/main/java/com/cemear/fila_conferencia_api/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.cemear.fila_conferencia_api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                // libera as origens do teu frontend web
+                .allowedOrigins(
+                        "http://localhost:3000",   // React local
+                        "http://localhost:5173"    // Vite (se usar)
+                        // adiciona aqui o domínio em produção depois
+                        // "https://fila-conferencia-web.vercel.app"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
